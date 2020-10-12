875992d4ecd1f02bedd41784032ecc50bb7bdbe0
Ashraful Islam <ashraful0103@gmail.com>
2020-10-12 14:48:38 +0600

    View Model information noted

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using EmployeeManagement.Models;
+using EmployeeManagement.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -24,11 +25,13 @@ namespace EmployeeManagement.Controllers
         }
         public ViewResult Details()
         {
-            Employee employeeModel = new Employee();
-            employeeModel = _repository.GetEmployee(1);
+            var homeDetailsViewModel = new HomeDetailsViewModel()
+            {
+                Employee = _repository.GetEmployee(1),
+                PageTitle = "Employee model"
+            };
 
-            ViewBag.PageTitle = "Employee model";
-            return View(employeeModel);
+            return View(homeDetailsViewModel);
         }
 
     }
