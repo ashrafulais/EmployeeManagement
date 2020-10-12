68c180d1c3cdacb184630d16ce8b85eaf242e62b
Ashraful Islam <ashraful0103@gmail.com>
2020-10-12 14:35:08 +0600

    StronglyTyped View Added

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IEmployeeRepository _repository { get; }
+
+        public HomeController(IEmployeeRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ViewResult Index()
+        {
+            return View();
+
+            //return _repository.GetEmployee(1).Name;
+        }
+        public ViewResult Details()
+        {
+            Employee employeeModel = new Employee();
+            employeeModel = _repository.GetEmployee(1);
+
+            ViewBag.PageTitle = "Employee model";
+            return View(employeeModel);
+        }
+
+    }
+}
---
+++ Models/MockEmployeeRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private readonly List<Employee> employees = new List<Employee>();
+
+        public MockEmployeeRepository()
+        {
+            employees = new List<Employee>()
+            {
+                new Employee()
+                {
+                    Id=1, Name="Sam", Department="CSE", Email="mail@mail.com"
+                },
+                new Employee()
+                {
+                    Id=2, Name="Mun", Department="MED", Email="mail@mail.com"
+                }
+            };
+        }
+
+        public Employee GetEmployee(int Id)
+        {
+            return employees.FirstOrDefault(x => x.Id == Id);
+        }
+    }
+}
