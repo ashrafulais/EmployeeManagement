26a19f5f0b8a6d4269dda0ef375fddb1061e34f4
Ashraful Islam <ashraful0103@gmail.com>
2020-10-22 13:23:41 +0600

    Singleton, Scoped, Transient

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,18 @@ namespace EmployeeManagement.Models
     public class Employee
     {
         public int Id { get; set; }
+        [Required, 
+         MaxLength(80, ErrorMessage = "Name cannot exceed 80 characters"),
+         MinLength(2, ErrorMessage ="Name must contain more than 1 character")]
         public string Name { get; set; }
+
+        [Required, 
+         RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", 
+            ErrorMessage="Invalid email format")]
+        [Display(Name = "Office Email")]
         public string Email { get; set; }
-        public DepartmentEnum Department { get; set; }
+        [Required]
+        public DepartmentEnum? Department { get; set; }
 
 
     }
