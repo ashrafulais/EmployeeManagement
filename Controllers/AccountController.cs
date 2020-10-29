c297d4f85ec3c1095994f1f2d58e0c5983c80c57
Ashraful Islam <ashraful0103@gmail.com>
2020-10-29 11:40:24 +0600

    Basic registration view added

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeManagement.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel register)
+        {
+            if(ModelState.IsValid)
+            {
+                //okay
+                return null;
+            }
+            return View();
+        }
+    }
+}
---
+++ ViewModels/RegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password",
+            ErrorMessage ="Password and the Confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
