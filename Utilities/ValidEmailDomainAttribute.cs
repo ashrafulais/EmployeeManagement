f30f33c678953086f60f22c388911b72c90752fa
Ashraful Islam <ashraful0103@gmail.com>
2020-11-14 14:17:16 +0600

    custom validation attribute

---
+++ Utilities/ValidEmailDomainAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Utilities
+{
+    public class ValidEmailDomainAttribute : ValidationAttribute
+    {
+        private readonly string allowedDomain;
+
+        public ValidEmailDomainAttribute(string allowedDomain)
+        {
+            this.allowedDomain = allowedDomain;
+        }
+        public override bool IsValid(object value)
+        {
+            return value.ToString().Split('@')[1].ToUpper() ==
+                allowedDomain;
+            //return base.IsValid(value);
+        }
+    }
+}
