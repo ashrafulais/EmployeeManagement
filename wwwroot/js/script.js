8cf5a36698e688e12ca6c6f3fca98d7531eeaf61
Ashraful Islam <ashraful0103@gmail.com>
2020-11-17 18:58:56 +0600

    Deletion confirmation

--- wwwroot/js/script.js
+++ wwwroot/js/script.js
@@ -1,1 +1,16 @@
 ﻿
+function confirmDelete(uniqueId, isDeleteClicked) {
+    var deleteSpan = "#deletespan_" + uniqueId;
+    var confirmDeleteSpan = "#confirmdeletespan_" + uniqueId;
+
+    $(deleteSpan).toggle();
+    $(confirmDeleteSpan).toggle();
+
+    /*if (isDeleteClicked) {
+        $(deleteSpan).hide();
+        $(confirmDeleteSpan).show();
+    } else {
+        $('#' + deleteSpan).show();
+        $('#' + confirmDeleteSpan).hide();
+    }*/
+}
